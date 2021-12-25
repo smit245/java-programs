823c790595f9f6131a5c7229b5dcd84c59246132
Smit Shah <49567481+smit245@users.noreply.github.com>
2021-12-25 19:33:32 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,10 +1,11 @@
+/*program is to find the number is armstrong or not like if i enter 153 then (1^3)+(5^3)+(3^3) should be equals 153*/
 import java.util.Scanner;
 
 public class Armstrong {
     public static void main(String[] ar)
     {
-        int n,rem=0,len=0;
-        int rearms=0;
+        int n,rem=0,len=0;//n is number.//rem is to get last digit of number .// len is to get number of digits of a number.
+        int rearms=0;//to store armstrong number.
         System.out.print("Enter a digit to check is it armstrong number or not :- ");
         Scanner sc = new Scanner(System.in);
         n=sc.nextInt();
@@ -19,9 +20,9 @@ public class Armstrong {
         int temp1=n;
         for(int i=0;i<len;i++)
         {
-            rem=temp1%10;
-            temp1=temp1/10;
-            rearms=rearms+(int)Math.pow(rem,len);
+            rem=temp1%10; // getting last digit
+            temp1=temp1/10; // removing last digit.
+            rearms=rearms+(int)Math.pow(rem,len); //doing the power of a number with number of digits.
         }
         System.out.println("===================================");
  
